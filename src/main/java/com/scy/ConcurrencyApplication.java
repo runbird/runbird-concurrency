db265be9d66ff42f7d7cf1e732b0e3519a7f2051
suocaiyuan <suocaiyuan@163.com>
2018-09-25 00:56:43 +0800

    add springboot2.0 Filter and Interceptor

--- src/main/java/com/scy/ConcurrencyApplication.java
+++ src/main/java/com/scy/ConcurrencyApplication.java
@@ -1,12 +1,31 @@
 package com.scy;
 
+import com.scy.threadlocal.HttpInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ConcurrencyApplication {
+@ServletComponentScan
+public class ConcurrencyApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(ConcurrencyApplication.class, args);
+    }
+
+//    @Bean
+//    public FilterRegistrationBean<Filter> httpFilter() {
+//        FilterRegistrationBean<Filter> registrationBean = new FilterRegistrationBean<>();
+//        registrationBean.setFilter(new HttpFilter());
+//        registrationBean.addUrlPatterns("/threadLocal/*");
+//        return registrationBean;
+//    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HttpInterceptor())
+                .addPathPatterns("/**");
     }
 }
