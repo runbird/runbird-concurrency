b186d0a9aadcae5a2d46018deee92207d51c981f
suocaiyuan <suocaiyuan@163.com>
2019-11-02 12:59:24 +0800

    add threadpool samples

--- src/main/java/com/scy/cache/CacheController.java
+++ src/main/java/com/scy/cache/CacheController.java
@@ -1,5 +1,6 @@
 package com.scy.cache;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CacheController {
 
     private final RedisClient redisClient;
-
+    @Autowired
     public CacheController(RedisClient redisClient) {
         this.redisClient = redisClient;
     }
---
+++ src/main/java/com/scy/utils/ThreadPoolTest.java
@@ -0,0 +1,36 @@
+package com.scy.utils;
+
+import java.util.concurrent.*;
+
+/**
+ * 类名： ThreadPoolTest <br>
+ * 描述：TODO <br>
+ * 创建日期： 2019/10/23 <br>
+ *
+ * @author suocaiyuan
+ * @version V1.0
+ */
+public class ThreadPoolTest {
+
+    public static void main(String[] args) {
+        LinkedBlockingQueue<Runnable> queue = new LinkedBlockingQueue<>(2);
+        RunbirdRejectHandler handler = new RunbirdRejectHandler();
+        RunbirdThreadFactory threadFactory1 = new RunbirdThreadFactory("第一机房");
+        RunbirdThreadFactory threadFactory2 = new RunbirdThreadFactory("第二机房");
+
+        ThreadPoolExecutor threadPoolExecutor1 =
+                new ThreadPoolExecutor(2, 4, 60L,
+                        TimeUnit.SECONDS, queue, threadFactory1, handler);
+
+        ThreadPoolExecutor threadPoolExecutor2 =
+                new ThreadPoolExecutor(2, 4, 60L,
+                        TimeUnit.SECONDS, queue, threadFactory1, handler);
+
+        Task task = new Task();
+        for (int i = 0; i < 100; i++) {
+            threadFactory1.newThread(task);
+            threadFactory2.newThread(task);
+        }
+    }
+
+}
