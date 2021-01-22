e4a30606dc34997dfe823e3a823fe91abd1b98dc
suocaiyuan <suocaiyuan@163.com>
2021-01-23 02:43:56 +0800

    AQS demo

---
+++ src/main/java/com/scy/juc/aqs/MLock.java
@@ -0,0 +1,76 @@
+package com.scy.juc.aqs;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @desc: 使用AQS实现自己的Lock
+ * @program: concurrency
+ * @author: Suocaiyuan
+ * @date: 2021-01-23 02:32
+ **/
+public class MLock implements Lock {
+
+    private Syn syn = new Syn();
+
+    @Override
+    public void lock() {
+        syn.acquire(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public void unlock() {
+        syn.release(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+
+    private class Syn extends AbstractQueuedSynchronizer {
+        @Override
+        protected boolean tryAcquire(int arg) {
+            assert arg == 1;
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            assert arg == 1;
+            if (!isHeldExclusively()) {
+                throw new IllegalMonitorStateException();
+            }
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        @Override
+        protected boolean isHeldExclusively() {
+            return getExclusiveOwnerThread() == Thread.currentThread();
+        }
+    }
+
+}
