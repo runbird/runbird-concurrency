44531f5099ec94d7ce6566a4d2323042d9d5de3b
suocaiyuan <suocaiyuan@163.com>
2021-08-27 22:42:47 +0800

    single

--- src/main/java/com/scy/demo/singleton/SingletonDemo.java
+++ src/main/java/com/scy/demo/singleton/SingletonDemo.java
@@ -10,6 +10,7 @@ import com.scy.annoations.ThreadSafe;
  *  --》推荐：相比懒汉模式线程更加安全，相比饿汉，不浪费资源
  *
  * 创建日期： 2018/9/20 <br>
+ *
  * @author suocaiyuan
  * @version V1.0
  */
@@ -20,28 +21,15 @@ public class SingletonDemo {
     private SingletonDemo() {
     }
 
+    public static SingletonDemo instance = new SingletonDemo();
+
     public static SingletonDemo getInstance() {
-        return Single.INSTANCE.getInstance();
-    }
-
-    private enum Single {
-        INSTANCE;
-
-        private SingletonDemo instance;
-
-        //JVM保证该构造函数只调用一次
-        Single() {
-            instance = new SingletonDemo();
-        }
-
-        public SingletonDemo getInstance() {
-            return instance;
-        }
+        return instance;
     }
 
     public static void main(String[] args) {
-        SingletonDemo singletonDemo = new SingletonDemo().getInstance();
-        SingletonDemo singletonDemo2 = new SingletonDemo().getInstance();
+        SingletonDemo singletonDemo = getInstance();
+        SingletonDemo singletonDemo2 = getInstance();
         System.out.println(singletonDemo.hashCode());
         System.out.println(singletonDemo2.hashCode());
     }
